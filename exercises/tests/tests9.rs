a5493b0751f9155c1794a5142ad273aafa03688f
Ferris <suring-13@foxmail.com>
2025-09-25 21:36:06 +0800

    update

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -28,15 +28,16 @@
 // You should NOT modify any existing code except for adding two lines of attributes.
 
 extern "Rust" {
-    #[link_name = "my_demo_function"]
+    #[link_name = "Foo::my_demo_function"]
     fn my_demo_function(a: u32) -> u32;
-    #[link_name = "my_demo_function"]
+    #[link_name = "Foo::my_demo_function"]
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
 mod Foo {
     // No `extern` equals `extern "Rust"`.
     #[no_mangle]
+    #[export_name = "Foo::my_demo_function"]
     fn my_demo_function(a: u32) -> u32 {
         a
     }
